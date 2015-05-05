960b8a54b66e45801bdc404c0becc339467ac98f
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-05-05 13:20:21 -0700

    add check for /dev/fb1

--- src/start.sh
+++ src/start.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 udevd --daemon
 udevadm trigger
-modprobe spi-bcm2708
-modprobe fbtft_device name=pitft verbose=0 rotate=270
 
+if [ ! -c /dev/fb1 ]; then
+  modprobe spi-bcm2708
+  modprobe fbtft_device name=pitft verbose=0 rotate=270
+
+  sleep 1
+
+  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
+fi
 
 echo "starting python script"
 python /usr/src/app/stockPrice.py
